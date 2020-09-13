f7f81fde0af6be66fb474a39b9022874b558402f
JavierCervilla <javier@cervilla.es>
2020-09-14 00:47:42 +0200

    initial commit, express working, conection with mongodb success and jitsi dnjs connected with api

---
+++ configs/views.configs.js
@@ -0,0 +1,14 @@
+const favicon = require('serve-favicon');
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+module.exports = app => {
+    app.engine(".hbs", exphbs({
+        defaultlayout: "layout",
+        extname : ".hbs"
+    }))
+    app.set("view engine", ".hbs");
+    app.use(express.static(path.join(__dirname, '..', 'public')))
+    app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
+}
